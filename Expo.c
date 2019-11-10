168ad446942d5fe3c7bd79ed9910904802be623e
Romulo de Vasconcelos Feijao Filho <romulofilho@Romulos-MacBook-Pro.local>
2019-11-10 13:07:03 -0300

    Agua para as pessoas da fila

--- Expo.c
+++ Expo.c
@@ -3,12 +3,12 @@
 #include <unistd.h>
 #include <pthread.h>
 
-
-#define PNE 3 //numero de pessoas com necessidades especiais
-#define PREF 5  //numero de pessoas preferenciais 
-#define NG 8 //numero de pessoas que não se encaixam nos requisitos acima (geral)
-#define capacidadeSALA 3 //numero de capacidade sala disponíveis na sala
-#define numeroSALA 3 //numero de salas na exposição
+#define PNE 4 //numero de pessoas com necessidades especiais
+#define PREF 6  //numero de pessoas preferenciais 
+#define NG 7 //numero de pessoas que não se encaixam nos requisitos acima (geral)
+#define CAPACIDADESALA 2 //numero de capacidade sala disponíveis na sala
+#define NUMEROSALA 3 //numero de salas na exposição
+#define COPOSAGUA 4 //numero de copos d'agua que o garçom tem 
 
 void * especiais(void * meuid); //declaração de protipagem da função
 void * preferencial (void * meuid); //declaração de protipagem da função
@@ -20,32 +20,35 @@ pthread_cond_t pref_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t geral_cond = PTHREAD_COND_INITIALIZER;
 
 // numero de capacidade sala ocupados e numero de pessoas querendo entrar na sala
-int numOCUPADOS[numeroSALA]; 
-int numPNE[numeroSALA];
-int numPREF[numeroSALA];
-int numGERAL[numeroSALA];
+int aguaGarcom[NUMEROSALA];
+int numOCUPADOS[NUMEROSALA]; 
+int numPNE[NUMEROSALA];
+int numPREF[NUMEROSALA];
+int numGERAL[NUMEROSALA];
 
 
 int main(int argc, char *argv[]){
 
   int j = 0;
 
-  for(j = 0; j < numeroSALA; j++)
+  for(j = 0; j < NUMEROSALA; j++)
     numOCUPADOS[j] = 0;
 
-  for(j = 0; j < numeroSALA; j++)
+  for(j = 0; j < NUMEROSALA; j++)
     numPNE[j] = 0;
 
-  for(j = 0; j < numeroSALA; j++)
+  for(j = 0; j < NUMEROSALA; j++)
     numPREF[j] = 0;
 
-  for(j = 0; j < numeroSALA; j++)
+  for(j = 0; j < NUMEROSALA; j++)
     numGERAL[j] = 0;
+
+  for(j = 0; j < NUMEROSALA; j++)
+    aguaGarcom[j] = COPOSAGUA;
 
   int erro;
   int i; //contador pra for's
   int *id; //id da thread
-  pthread_t princ;
 
   pthread_t tPNE[PNE]; //cria thread
   for (i = 0; i < PNE; i++){ //cria pne threads
@@ -86,9 +89,18 @@ int main(int argc, char *argv[]){
     }
   }
 
+  for(j = 0; j < PNE; j++){
+    pthread_join(tPNE[j],NULL);
+  }
+
+  for(j = 0; j < PREF; j++){
+    pthread_join(tPREF[j],NULL);
+  }
+
   for(j = 0; j < NG; j++){
     pthread_join(tNG[j],NULL);
   }
+
   return 0;
 } 
 
@@ -98,7 +110,13 @@ void * especiais (void* pid){
     pthread_mutex_lock(&mutex);
     printf("Pessoa PNE %d: Estou na fila da sala %d da exposição.  \n", *(int *)(pid), salaAtual);
     numPNE[salaAtual]++;
-    while(numOCUPADOS[salaAtual] == capacidadeSALA) {
+    while(numOCUPADOS[salaAtual] == CAPACIDADESALA) {
+      if(aguaGarcom[salaAtual] > 0){
+        aguaGarcom[salaAtual]--;
+        printf("Garçom da fila da sala %d: Dando agua para Pessoa PNE %d, existem %d águas restantes \n", salaAtual, *(int *)(pid), aguaGarcom[salaAtual]);
+      }else{
+        printf("O garçom da fila %d não tem mais água para as pessoas da fila \n", salaAtual);
+      }
       pthread_cond_wait(&pne_cond,&mutex);
     }
     numPNE[salaAtual]--;
@@ -113,7 +131,7 @@ void * especiais (void* pid){
     printf("Pessoa PNE %d: Saí da sala %d. Numº pessoas na sala da exposição %d = %d\n", *(int *)(pid), salaAtual, salaAtual, numOCUPADOS[salaAtual]);
     salaAtual++;
 
-    if(salaAtual == numeroSALA){
+    if(salaAtual == NUMEROSALA){
       pthread_mutex_unlock(&mutex);
       pthread_exit(NULL);
     }
@@ -133,7 +151,13 @@ void * preferencial (void* pid){
     pthread_mutex_lock(&mutex);
     printf("Pessoa Preferencial %d: Estou na fila da sala %d da exposição. \n", *(int *)(pid), salaAtual);
     numPREF[salaAtual]++;
-    while(numOCUPADOS[salaAtual] == capacidadeSALA || numPNE[salaAtual] > 0) {
+    while(numOCUPADOS[salaAtual] == CAPACIDADESALA || numPNE[salaAtual] > 0) {
+      if(aguaGarcom[salaAtual] > 0){
+        aguaGarcom[salaAtual]--;
+        printf("Garçom da fila da sala %d: Dando agua para Pessoa Preferencial %d, existem %d águas restantes \n", salaAtual, *(int *)(pid), aguaGarcom[salaAtual]);
+      }else{
+        printf("O garçom da fila %d não tem mais água para as pessoas da fila \n", salaAtual);
+      }
       pthread_cond_wait(&pref_cond,&mutex);
     }
     numPREF[salaAtual]--;
@@ -148,7 +172,7 @@ void * preferencial (void* pid){
     printf("Pessoa Preferencial %d: Saí da sala %d. Numº pessoas na sala da exposição %d = %d\n", *(int *)(pid), salaAtual, salaAtual, numOCUPADOS[salaAtual]);
     salaAtual++;
 
-    if(salaAtual == numeroSALA){
+    if(salaAtual == NUMEROSALA){
       pthread_mutex_unlock(&mutex);
       pthread_exit(NULL);
     }
@@ -169,7 +193,13 @@ void * geral (void* pid){
     printf("Pessoa Geral %d: Estou na fila da sala %d da exposição.  \n", *(int *)(pid), salaAtual);
      
     numGERAL[salaAtual]++;
-      while(numOCUPADOS[salaAtual] == capacidadeSALA || numPNE[salaAtual] > 0 || numPREF[salaAtual] > 0) {
+    while(numOCUPADOS[salaAtual] == CAPACIDADESALA || numPNE[salaAtual] > 0 || numPREF[salaAtual] > 0) {
+      if(aguaGarcom[salaAtual] > 0){
+        aguaGarcom[salaAtual]--;
+        printf("Garçom da fila da sala %d: Dando agua para Pessoa Geral %d, existem %d águas restantes \n", salaAtual, *(int *)(pid), aguaGarcom[salaAtual]);
+      }else{
+        printf("O garçom da fila %d não tem mais água para as pessoas da fila \n", salaAtual);
+      }
       pthread_cond_wait(&geral_cond,&mutex);
     }
     numGERAL[salaAtual]--;
@@ -184,7 +214,7 @@ void * geral (void* pid){
     printf("Pessoa Geral %d: Saí da sala %d. Numº pessoas na sala de exposição %d = %d\n", *(int *)(pid), salaAtual, salaAtual, numOCUPADOS[salaAtual]);
     salaAtual++;
 
-    if(salaAtual == numeroSALA){
+    if(salaAtual == NUMEROSALA){
       pthread_mutex_unlock(&mutex);
       pthread_exit(NULL);
     }
